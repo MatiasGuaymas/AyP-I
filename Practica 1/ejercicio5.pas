6143e35e7e81447a79a781da4dc479bb3b9612ef
Matias Guaymas <matiasguaymas2005@gmail.com>
2025-03-03 17:48:52 -0300

    Practica 1

---
+++ Practica 1/ejercicio5.pas
@@ -0,0 +1,21 @@
+{5. Realice un programa que informe el valor total en pesos de una transacción en dólares. Para ello, el programa
+debe leer el monto total en dólares de la transacción, el valor del dólar al día de la fecha y el porcentaje (en
+pesos) de la comisión que cobra el banco por la transacción. Por ejemplo, si la transacción se realiza por 10
+dólares, el dólar tiene un valor 863,50 pesos y el banco cobra un 4% de comisión, entonces el programa deberá
+informar:
+La transacción será de 8980.4 pesos argentinos (resultado de multiplicar 10*863,50 y adicionarle el 4%)}
+
+program ejercicio5;
+var
+    monto, valor, porcentaje, transaccion: real;
+begin
+    writeln('Ingrese el monto total en dolares');
+    readln(monto);
+    writeln('Ingrese el valor del dolar al dia de la fecha');
+    readln(valor);
+    writeln('Ingrese el porcentaje de la comision que cobra el banco por la transaccion');
+    readln(porcentaje);
+    transaccion:= (monto *  valor) + (monto *  valor * porcentaje) / 100;
+    writeln('La transaccion sera de: ', transaccion:0:2);
+end.
+
